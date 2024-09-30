628bc9190876522420c5643e1edf2fc65cf7f287
Bheemesh Gadige <gbheemesh888@gmail.com>
2024-09-30 20:22:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -9,6 +9,15 @@ var sum = 0;
 for (let i = 0; i < total.length; i++) {
     sum += parseFloat(total[i].innerText);
 }
+
+	
+	const table = document.getElementsByTagName("table")[0];
+	const newRow = document.createElement('tr');
+	const newCell = document.createElement('td');
+	newCell.innerText = sum;
+	newRow.appendChild(newCell);
+	table.appendChild(newRow);
+	
 return sum;
 };
 
