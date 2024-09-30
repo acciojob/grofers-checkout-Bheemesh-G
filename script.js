6f3bd2f25ff04bca94b7b6cf06b399b9ed8693f2
Bheemesh Gadige <gbheemesh888@gmail.com>
2024-09-30 20:11:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,7 +4,12 @@ document.body.appendChild(getSumBtn);
 
 const getSum = () => {
 //Add your code here
-  
+	const total = document.querySelectorAll(".price");;
+var sum = 0;
+for (let i = 0; i < total.length; i++) {
+    sum += parseFloat(total[i].innerText);
+}
+return sum;
 };
 
 getSumBtn.addEventListener("click", getSum);
